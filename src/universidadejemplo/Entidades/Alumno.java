e3bc9f5a7c9dd90fcfa30d4146801df759c117fb
markowonka <141674451+markowonka@users.noreply.github.com>
2023-09-11 17:06:08 -0300

    clases entidad
    clases entidad

--- src/universidadejemplo/Entidades/Alumno.java
+++ src/universidadejemplo/Entidades/Alumno.java
@@ -5,10 +5,82 @@
  */
 package universidadejemplo.Entidades;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author scscl
  */
 public class Alumno {
+    
+    int idalumno;
+    String nombre;
+    String apellido;
+    LocalDate fechadenacimiento;
+    boolean activo;
+
+    public Alumno(int idalumno, String nombre, String apellido, LocalDate fechadenacimiento, boolean activo) {
+        this.idalumno = idalumno;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechadenacimiento = fechadenacimiento;
+        this.activo = activo;
+    }
+
+    public Alumno(String nombre, String apellido, LocalDate fechadenacimiento, boolean activo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechadenacimiento = fechadenacimiento;
+        this.activo = activo;
+    }
+
+    public int getIdalumno() {
+        return idalumno;
+    }
+
+    public void setIdalumno(int idalumno) {
+        this.idalumno = idalumno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDate getFechadenacimiento() {
+        return fechadenacimiento;
+    }
+
+    public void setFechadenacimiento(LocalDate fechadenacimiento) {
+        this.fechadenacimiento = fechadenacimiento;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idalumno=" + idalumno + ", nombre=" + nombre + ", apellido=" + apellido + ", fechadenacimiento=" + fechadenacimiento + ", activo=" + activo + '}';
+    }
+    
+    
+    
+    
     
 }
--- src/universidadejemplo/Entidades/Inscripcion.java
+++ src/universidadejemplo/Entidades/Inscripcion.java
@@ -11,4 +11,56 @@ package universidadejemplo.Entidades;
  */
 public class Inscripcion {
     
+    int idinscripcion;
+    Alumno alumno;
+    Materia materia;
+    double nota;
+
+    public Inscripcion(int idinscripcion, Alumno alumno, Materia materia, double nota) {
+        this.idinscripcion = idinscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public int getIdinscripcion() {
+        return idinscripcion;
+    }
+
+    public void setIdinscripcion(int idinscripcion) {
+        this.idinscripcion = idinscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
+    
+    
 }
