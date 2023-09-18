09075b7499fbee74315f0283216d9882da8b1595
killerchipy723 <diegoeduardo66@gmail.com>
2023-09-18 15:12:22 -0300

    regenerated

--- src/universidadejemplo/AccesoADatos/InscipcionData.java
+++ src/universidadejemplo/AccesoADatos/InscipcionData.java
@@ -1,27 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadejemplo.AccesoADatos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import universidadejemplo.Entidades.Inscripcion;
 
-/**
- *
- * @author Marcoooooo
- */
 public class InscipcionData {
-    Conexion con;
-    MateriaData MatData;
-    AlumnoData Aludata;
+  
 
-    public InscipcionData() {
+    public void InscipcionData() {
+      
     }
     
     public void GuardarInscripcion(Inscripcion ins){
-        
+         Connection con = Conexion.Conectar();
+        String slq = "INSERT INTO inscripcion(idAlumno,nombre,nota)VALUES(?,?,?)";
+        try {
+            PreparedStatement pst = con.prepareStatement(slq,Statement.RETURN_GENERATED_KEYS);
+            pst.setInt(1,ins.getAlumno().getIdalumno());
+            pst.setInt(2,ins.getMateria().getIdmateria());
+            pst.setDouble(3, ins.getNota());
+            pst.executeUpdate();
+            ResultSet rs = pst.getGeneratedKeys();
+            if(rs.next()){
+                ins.setIdinscripcion(rs.getInt(1));
+                JOptionPane.showMessageDialog(null,"Inscripcion Exitosa");
+                
+            }
+            
+            pst.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Erro al conectar"+ ex);
+        }
     }
     
 //    public List <Inscripcion> ObtenerInscripciones(){
